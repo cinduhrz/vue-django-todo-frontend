1b0ad48f29dac481c9fd1e222c6ab23154280ba9
Cindy Wong <cindywongdev@gmail.com>
2023-03-13 18:09:25 -0400

    created router and routes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import routes from './routes'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    // create a web history
+    history: createWebHistory(),
+    // inject our routes
+    routes
+})
+
+// create vue app
+createApp(App)
+// inject router into app
+.use(router)
+// mount app to the DOM
+.mount('#app')
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+/////////////////////////
+// Components Imports
+/////////////////////////
+
+
+/////////////////////////
+// Array of Routes
+/////////////////////////
+export default [
+    { path: "/", component: Home },
+    { path: "/about", component: About }
+];
