38e1db427b6894cf68f51f383cbf99b1950261ca
Cindy Wong <cindywongdev@gmail.com>
2023-03-18 13:56:42 -0400

    delete function added

--- src/pages/SinglePost.vue
+++ src/pages/SinglePost.vue
@@ -1,31 +1,50 @@
 <template>
     <div class="post-page">
+        <router-link to="/"><button>Back to Main</button></router-link>
         <h1>{{ todo.subject }}</h1>
         <h2>{{ todo.details }}</h2>
         <router-link :to="{name: 'edit', params: {id: todo.id}}"><button>Edit Todo</button></router-link>
-        <router-link to="/"><button>Back to Main</button></router-link>
+        <button v-on:click="deletePost">Delete Todo</button>
     </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router' // get useRoute hook to get access to route params
+import { useRoute, useRouter } from 'vue-router' // get useRoute hook to get access to route params
 import { toRefs } from 'vue' // get toRegs hook to maintain props reactivity
 
 // define props and their type
 const props = defineProps({
-    'todos': Array
+    'todos': Array,
+    'url': String,
+    'getTodos': Function
 })
 
+// get router to use router.push
+const router = useRouter()
 // get route object to access params
 const route = useRoute()
-// retrieve posts from props
-const { todos } = toRefs(props)
+// retrieve needed props
+const { todos, url, getTodos } = toRefs(props)
 // grab target post from posts
 // can't use strict equality (===) here bc it throws an error, i guess the type is diff?
 const todo = todos.value.find((todo) => {return todo.id == route.params.id})
+
+const deletePost = async function() {
+    await fetch(url.value + todo.id + "/", {
+        method: "delete",
+    })
+
+    // rerun to update, also async function
+    await getTodos.value()
+    router.push("/")
+}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+button {
+    margin: 10px;
+}
+</style>
 
 
 <!-- export default {
